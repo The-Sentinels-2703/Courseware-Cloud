6c658703902684cd6762a50f3886d1154d054dec
The-Sentinels-2703 <thesentinels24@gmail.com>
2025-03-24 04:27:25 -0700

    project environment setup

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import userRoutes from './routes/userRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
+app.use('/', userRoutes);
+app.use('/materials', uploadRoutes)
 
 app.listen(3024, ()=>{
     console.log("server running at http://localhost:3024");
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router()
 
 router.post('new', (req, res) => {
-  
+    
 })
 
 export default router;
