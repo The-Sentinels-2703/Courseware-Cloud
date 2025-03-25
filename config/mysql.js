cf49f74fffe7c8be525989f8c1bf63acc5934d54
The-Sentinels-2703 <thesentinels24@gmail.com>
2025-03-25 01:58:22 -0700

    database setup

--- config/mysql.js
+++ config/mysql.js
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
+import process from 'process';
 
-const databaseName = process.env.DBNAME;
-const databaseUserName = process.env.DBUSERNAME;
-const databasePassword = process.env.DBPASSWORD;
+const databaseName = process.env.DB_NAME;
+const databaseUserName = process.env.DB_USER;
+const databasePassword = process.env.DB_PASSWORD;
 const sequelize = new Sequelize(databaseName, databaseUserName, databasePassword, {
   host: '127.0.0.1',
   dialect: 'mysql',
